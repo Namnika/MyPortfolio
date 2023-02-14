cbf7a7cda7395534a7fddcb6e2af0f246f30c948
Namnika <janbandhunamnika@gmail.com>
2023-02-15 01:32:25 +0530

    Updating files

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -65,7 +65,7 @@ export default function Contact() {
         "Content-Type": "application/json",
       };
       axios
-        .post("/", data, headers)
+        .post("https://backend-portfolio-rosy.vercel.app/#contact", data, headers)
         .then((res) => {
           console.log(res.data);
         })
