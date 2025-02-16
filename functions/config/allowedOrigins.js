b9d0d6bacc18ee6a3e4f6ba0dd203ab4d37a59ed
Namnika <janbandhunamnika@gmail.com>
2025-02-16 19:18:56 +0530

    add new origin

--- functions/config/allowedOrigins.js
+++ functions/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "https://namnika-janbandhu.com",
     "https://www.namnika-janbandhu.com",
+    "https://namnika-janbandhu-portfolio.vercel.app",
     "https://backend-portfolio-rosy.vercel.app"
 ];
 
